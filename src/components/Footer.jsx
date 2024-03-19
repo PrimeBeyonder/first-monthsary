87b581b52cb18b60466430994169976da4a0221c
Andrew <thum14252@gmail.com>
2024-03-19 14:38:01 +0630

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ import "../Css/Footer.css";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer id="footer" className="footer">
+            <div className="footer__container container">
+                <h1 className="footer__title">Me  <i class="uil uil-heart-alt"></i> MaMaHsu</h1>
+                <span className="footer__copy">&#169; Andrew. All Right Reserved</span>
+            </div>
+        </footer>
     )
 }
 
